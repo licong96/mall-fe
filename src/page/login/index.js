c1b46480a4fb528076de5c28404a85321493643b
licong96 <licong19961204@.gmail.com>
2018-03-05 14:24:40 +0800

    添加result

--- src/page/login/index.js
+++ src/page/login/index.js
@@ -1,1 +1,3 @@
 import './login.scss';
+
+// login 登录
--- src/page/result/index.js
+++ src/page/result/index.js
@@ -3,3 +3,22 @@ require('../common/common.js')
 
 require('page/layout/nav-simple/index.js');
 require('page/layout/footer/index.js');
+
+// result 操作成功状态
+
+var _mm = require('util/mm.js');
+
+var result = {
+  init: function () {
+    this.onLoader();
+  },
+  onLoader: function () {
+    // 根据type显示哪条提示信息
+    var type = _mm.getUrlParam('type') || 'default',
+        $element = $('.' + type + '-success');
+
+    $element.show();
+  }
+}
+
+result.init()
